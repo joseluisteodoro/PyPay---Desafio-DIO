74624ccf13c23fa7f56fa1d9d9bfa9a5b72d6e07
joseluisteodoro <joseltodoro2@gmail.com>
2025-06-05 07:55:38 -0300

    Add feature main_file

---
+++ PyPay.py
@@ -0,0 +1,8 @@
+#Este é o sistema bancário criado para o desafio da DIO
+#O desafio consiste em criar um sistema bancário em que:
+
+#Deve possuir 3 funções: Saque, Depósito e Extrato
+#O valores retornado em extrato deve ser no formato R$ 100,00
+#Cada saque e depósito deve ser armazenado no extrato devidamente identificado
+#Exibir mensagens de erro caso o saldo seja insuficiente e demais erros possíveis
+#Possuir Menu - E não é necessário possuir usuário
