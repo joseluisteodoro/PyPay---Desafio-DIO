017465108189b975d4872535fc846cbc01fba01c
joseluisteodoro <joseltodoro2@gmail.com>
2025-06-06 01:40:31 -0300

    update main_file e README v1 finalizada e readme atualizado

--- PyPay.py
+++ PyPay.py
@@ -13,8 +13,8 @@ from tkinter import Menu
 
 print("""
 ++++++++++++++++++++++++++++++++++++      
-     Olá! Seja Bem ao PyPay!
-O melhor banco digital em Pyhon!
+     Olá! Seja Bem ao PyPay!🌐
+O melhor banco digital em Pyhon!🐍
       
 +++++++++Version: V1.0++++++++++++++
       """)
@@ -24,13 +24,13 @@ menu = """
 ************************************
                 MENU
 ************************************      
-     Selecione a opção desejada:
 
-[1] - Depositar
-[2] - Sacar
-[3] - Extrato
-[0] - Sair
+[1] - 📨 Depositar
+[2] - 💲 Sacar
+[3] - 🧾 Extrato
+[0] - ❌ Sair
 
+Selecione a opção desejada: 
 """
 
 #Iniciamos definindo valores base para o código
@@ -43,5 +43,85 @@ LIMITE_SAQUE = 3
 #Definimos while True para fazer um looping
 while True:
 
-    opcao = input(menu)
+    opcao = int(input(menu))
+    print("")
+
+    #Depósito
+    if opcao == 1:
+        valor = float(input("Informe o valor que deseja depositar: "))
+        if valor > 0:
+           saldo += valor
+           extrato += f"Depósito || R$ {valor:.2f}\n"
+           print(f"""
+Depósito de R$ {valor:.2f} realizado com sucesso!!
+Saldo atual R$ {saldo:.2f}""")
+        else:
+                  print("Falha! Informe um valor a partir de R$ 0,01")
+     
+     #Saque
+    elif opcao == 2:
+         valor = float(input("Informe o valor que deseja sacar: "))
+         print()
+         
+         #definindo variáveis limitadoras do saque
+         saldo_insuficiente = valor > saldo
+         limite_insuficiente = valor > LIMITE
+         saques_excedido = saques_feitos >= LIMITE_SAQUE
+         
+         #Definindo condicionais de saque - É preciso definir antes os limites, e possíveis erros antes do saque ser autorizado
+         if saldo_insuficiente:
+              print(f"""
+Falha! Valor digitado maior que o saldo.
+Saldo atual R$ {saldo:.2f}
+                  """)
+         elif limite_insuficiente:
+              print(f"""
+Falha! Seu limite por saque é de R$ {LIMITE}.
+                  """)
+         elif saques_excedido:
+              print(f"""
+Falha! Número máximo de {LIMITE_SAQUE} saque(s) excedido.
+                  """)      
+                  
+          #Operação de saque    
+         elif valor > 0:
+              saldo -= valor
+              extrato += f"Saque    || R$ {valor:.2f}\n"
+              print(f"""
+Saque de R$ {valor:.2f} realizado com sucesso!!
+Saldo atual R$ {saldo:.2f}
+               """)
+              saques_feitos += 1
+
+    #Extrato          
+    elif opcao == 3:
+         if extrato == "":
+              print("""
+Nenhuma movimentação realizada!
+                    """)
+         else:
+              print(f"""
+************************************
+                Extrato
+************************************ 
+{extrato}
+
+************************************ 
+Saldo atual R$ {saldo:.2f}
+                """)
+     #definido break para parar o código         
+    elif opcao == 0:
+         print("""
+Muito obrigado por usar o PyPay!🌐
+✨Até logo!!✨""")
+         break
+    
+    else:
+         print("\n Digite uma opção válida! \n")
+    
+
+
+
+
+
 
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# PyPay---Desafio-DIO
-Este é o sistema bancário criado para o desafio da DIO
+
+# PyPay🌐
+
+Olá! O PyPay🌐 faz parte do desafio para a DIO.
+Se trata de uma versão simples de um sistema bancário feito em python 
+
+Versão atual V1.0
+
+## ✨ Funções disponíveis
+Saque  
+Depósito  
+Extrato
+
+## 📖 Tecnologias utilizadas
+![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54) 
+
+## ✍🏽 Changelog
+V1.0
+- Criação da base do PyPay🌐  
+- Adicionado menu  
+- Funções: Saque, Depósito e Extrato   
+- Ajustado Design e emojis
+
+
+## 🌐Conecte-se comigo
+[![LinkedIn](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/jos%C3%A9-luis-d-473a72136/) [![GitHub](https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white)](https://github.com/joseluisteodoro) [![Instagram](https://img.shields.io/badge/-Instagram-%23E4405F?style=for-the-badge&logo=instagram&logoColor=white)](https://www.instagram.com/joseluis.musica/)
